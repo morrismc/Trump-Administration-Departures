98c1dfde3c7d330a88d487a553ff8bf3262da6d0
Matthew_morriss <matthew.c.morriss@gmail.com>
2018-04-11 11:36:33 -0700

    licensing
    new licensing

--- Departures.R
+++ Departures.R
@@ -1,3 +1,19 @@
+# <Load Office of Public Managment Data and visualize the result.>
+#     Copyright (C) <2018>  <Matthew C Morriss>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #This Script will load all of the employment data from cabinent level agencies.
 setwd('/Users/matthew/Documents/GitHub/Trump_Administration_departures/TAD')
 library(dplyr)
